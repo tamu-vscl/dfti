36c1d48d1713bb8e9fb5455803491725cbf4b6d8
Joshua Harris <joshua.a.harris@tamu.edu>
2016-04-27 20:10:15 -0500

    Remove dead code, clean up.
    Remove leftover old code from pre-git development before the VN200
    class/lib was created. Use C-style comments, and generally clean up the
    code.

--- dfti.ino
+++ dfti.ino
@@ -4,12 +4,12 @@
  *  Reads in sensor values and stores to file.
  */
 
-// Arduino stdlib
+/* Arduino stdlib */
 #include <SPI.h>
 #include <SD.h>
-// Third-party libs
+/* Third-party libs */
 #include <Queue.h>
-// Custom libs
+/* Custom libs */
 #include <errno.h>
 #include <libbc.hh>
 #include <libvn200.hh>
@@ -19,9 +19,8 @@
 #define VN200_BAUD_RATE 115200
 #define UADC_BAUD_RATE 115200
 /* Configure SD card logging. */
-#define FLUSH_THRESHOLD 10000
 #define SD_CARD             4
-// Increment file to record the last log file number.
+/* Increment file to record the last log file number. */
 #define INCREMENT_FILE "lastlog.txt"
 /* Configure analog inputs. */
 #define LEFT_AILERON_PIN      1
@@ -42,164 +41,115 @@
 
 
 /* Global variables. */
-// Aliases for h/w serial ports to allow easily reconfiguration.
-HardwareSerial *VN200Serial = &Serial2;
-HardwareSerial *ADCSerial = &Serial3;
-// File object for log.
+/* Aliases for h/w serial ports to allow easily reconfiguration. */
+HardwareSerial *ADCSerial = &Serial3; // TODO: Replace with UADC object.
+/* File object for log. */
 File logfile = NULL;
-// Sensor objects.
+/* Sensor objects. */
 VN200 vn200(&Serial2, VN200_BAUD_RATE);
-// Task scheduling queue.
+/* Task scheduling queue. */
 Queue queue;
-// Flag to ensure successful initialization.
+/* Flag to ensure successful initialization. */
 uint8_t init_success = true;
 
 
 void
 setup()
 {
-    // Setup h/w serial devices.
+    /* Setup h/w serial devices. */
     vn200.begin();
     ADCSerial->begin(UADC_BAUD_RATE);
 
-    // Attempt to open the SD card and open the log file.
+    /* Attempt to open the SD card and open the log file. */
     if (!SD.begin(SD_CARD)) {
         init_success = false;
     } else {
-        if (setup_logfile() < 0) {
+        if (open_logfile() < 0) {
             init_success = false;
         }
     }
 
-    // Clear buffers.
-    memset(vn200_buf, 0, VN200_BUFSIZE);
-    memset(vn200data, 0, sizeof(struct VN200Data));
-
-    // Set up task scheduling.
-    // Flush data to file every FLUSH_THRESHOLD seconds.
-    queue.scheduleFunction(flush_logfile, "WRTLOG", 0, TASK_10SEC);
-    // Read in sensor data at 100 Hz.
-    queue.scheduleFunction(read_sensors, "GETDATA", 0, TASK_100HZ);
+    /* Flush data to file every 10 seconds. */
+    queue.scheduleFunction(flush_logfile, "FLUSHLOG", 0, TASK_10SEC);
+    /* Read in sensor data at 100 Hz and log. */
+    queue.scheduleFunction(sample_sensors, "SAMPDATA", 0, TASK_100HZ);
 }
 
 
 void
 loop()
 {
-    // Only run the loop code if we initialized successfully.
+    /* Only run the loop code if we initialized successfully. */
     if (init_success) {
-        // Start scheduler.
+        /* Start scheduler. */
         queue.Run(millis());
         delay(TASK_100HZ);
     }
 }
 
 
-DFTIError
-setup_logfile(void)
+/*
+ * Open log file and increment the last log number.
+ * Returns -EIO if files can't be opened for writing, 0 for success.
+ */
+uint8_t
+open_logfile(void)
 {
-    int8_t rv = 0;
+    int8_t rv = TASK_SUCCESS;
     uint8_t num = 0;
-    File last_log, current_log;
+    File log = NULL;
 
+    /*
+     * Attempt to open the increment file and read in the integer value for
+     * the last log file. Note that if the last_log file is empty, the return
+     * value will be zero and we just use this as the starting point. If the
+     * file doesn't exist, we'll just create it.
+     */
     if SD.exists(INCREMENT_FILE) {
-        /* Open the increment file and read in the integer value for the last
-         * log file. Note that if the last_log file is empty, the return value
-         * will be zero and we just use this as the starting point.
-         */
-        last_log = SD.open(INCREMENT_FILE, FILE_READ);
-        if (last_log.available()){
-            num = last_log.parseInt();
+        log = SD.open(INCREMENT_FILE, FILE_READ);
+        if (log.available()){
+            num = log.parseInt();
         }
-        last_log.close()
-        // Increment the log file number and write to the increment file.
-        ++num;
-        last_log = SD.open(INCREMENT_FILE, FILE_WRITE);
-        last_log.write(num);
-        last_log.close();
-        // Open file for logging.
-        logfile = open(sprintf("flightlog%03d.txt", num), FILE_WRITE);
-        if (!logfile) {
-            rv = -EIO;
-        }
-        // Set the pointer to the current log file.
-        return 0;
-    } else {
-        rv = -ENOENT;
+        log.close();
     }
 
+    /* Increment the log file number and write to the increment file. */
+    ++num;
+    log = SD.open(INCREMENT_FILE, FILE_WRITE);
+    if (!log) {
+        /* Failed to open increment file for writing. */
+        rv = -EIO;
+    }
+    log.write(num);
+    log.close();
+    log = NULL;
+
+    /* Open file for logging. */
+    logfile = open(sprintf("flightlog%03d.txt", num), FILE_WRITE);
+    if (!logfile) {
+        /* Failed to open new log file. */
+        rv = -EIO;
+    }
+
+    return rv;
 }
 
 
-// Callback function to flush the SD card write buffer.
+/* Callback function to flush the SD card write buffer. */
 int
 flush_logfile(unsigned int now)
 {
     if (logfile != NULL) {
         logfile.flush();
     }
-}
-
-
-int
-read_sensors(unsigned int now)
-{
-    // TODO: Call the sensor object read() methods.
     return TASK_SUCCESS;
 }
 
 
+/* Callback function to read in sensor data and write to logfile. */
 int
-read_adc(unsigned int now)
+sample_sensors(unsigned int now)
 {
-    if (ADCSerial->available()) {
-        // TODO: Read in serial data.
-    }
+    /* TODO: Call the sensor object read() methods. */
     return TASK_SUCCESS;
-}
-
-
-void
-parse_vn200(void)
-{
-    vn200data.yaw = bytes2float(vn200_buf, 4));
-    vn200data.pitch = bytes2float(vn200_buf, 8));
-    vn200data.roll = bytes2float(vn200_buf, 12));
-    vn200data.p = bytes2float(vn200_buf, 28));
-    vn200data.q = bytes2float(vn200_buf, 32));
-    vn200data.r = bytes2float(vn200_buf, 36));
-}
-
-
-float bytes2float(uint8_t *buf, uint8_t idx)
-{
-    // Assumes a 4-byte float representation.
-    uint8_t b[4] = {buf[idx + 3], buf[idx + 2], buf[idx + 1], buf[idx]};
-    float rv;
-    memset(&rv, &b, sizeof(float));
-    return rv;
-}
-
-
-float
-deg2rad(float d)
-{
-    return PI / 180 * d;
-}
-
-
-// TODO; Need to make sure we use the correct buffer parts.
-bool
-valid_vn200_pkt(uint8_t buf[], uint8_t len)
-{
-    /* Warning: FM here. */
-    uint16_t crc = 0x0000;
-    for (uint8_t i = 0; i < len; ++i) {
-        crc = (uint16_t)((uint8_t) (crc >> 8) | (crc << 8));
-        crc ^= (uint16_t) buf[i];
-        crc ^= (uint8_t) (crc & 0xff) >> 4;
-        crc ^= crc << 12;
-        crc ^= (crc & 0x00ff) << 5;
-    }
-    return crc == 0x0000 ? true : false;
 }
