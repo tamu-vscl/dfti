9a6bee179b484963623260db031d91f098f81cc5
Joshua Harris <josh@joshharris.org>
2016-04-29 17:39:19 -0500

    Fix file IO and scheduling.
    Both problems turned out to be that the task IDs and the SD card
    filenames were too long.

--- dfti.ino
+++ dfti.ino
@@ -12,7 +12,7 @@
 #include <Queue.h>
 /* Custom libs */
 #include <libsensor.h>
-#include <libadc.h>
+#include <libuadc.h>
 #include <libvn200.h>
 
 
@@ -20,7 +20,7 @@
 #define VN200_BAUD_RATE 115200
 #define UADC_BAUD_RATE 115200
 /* Configure SD card logging. */
-#define SD_CARD     8
+#define SD_CARD     4
 #define FN_BUFLEN  50
 /* Increment file to record the last log file number. */
 #define INCREMENT_FILE "lastlog.txt"
@@ -40,59 +40,70 @@
 #define TASK_1HZ      1000
 #define TASK_10HZ      100
 #define TASK_100HZ      10
+/* Debugging and failure states. */
+#define FAULT_SDIO        0x01
+#define FAULT_FILEIO      0x02
+#define FAULT_SCHED_FLUSH 0x04
+#define FAULT_SCHED_SAMPL 0x08
+#define DEBUG
 
 
 /* Global variables. */
 /* File object for log. */
-File logfile;
+File fd;
 /* Sensor objects. */
 VN200 vn200;
 UADC uadc;
 /* Task scheduling queue. */
 Queue queue;
 /* Flag to ensure successful initialization. */
-bool init_success = true;
+int8_t init_success = 0;
+/* String separator. */
+String sep = String(",");
 
 
 void
 setup()
 {
-
-#ifdef DEBUG
-    Serial.begin(9600);
-#endif
-
     /* Setup h/w serial devices. */
     vn200 = VN200(&Serial2, (uint8_t) VN200_BAUD_RATE);
     uadc = UADC(&Serial3, (uint8_t) UADC_BAUD_RATE);
     vn200.begin();
     uadc.begin();
-#ifdef DEBUG
-    Serial.printLn("Setup VN-200 and UADC.")
-#endif
 
     /* Attempt to open the SD card and open the log file. */
     if (!SD.begin(SD_CARD)) {
-        init_success = false;
+        init_success ^= FAULT_SDIO;
     } else {
         if (open_logfile() < 0) {
-            init_success = false;
+            init_success ^= FAULT_FILEIO;
         }
     }
-#ifdef DEBUG
-    if (init_success) {
-        Serial.printLn("Successfully connected to SD card.")
-    } else {
-        Serial.printLn("Failed to connect to SD card.")
-    }
-#endif
 
     /* Flush data to file every 10 seconds. */
-    queue.scheduleFunction(flush_logfile, "FLUSHLOG", 0, TASK_10SEC);
+    if (queue.scheduleFunction(flush_logfile, "FLUSH", 50, TASK_10SEC) < 0) {
+        init_success ^= FAULT_SCHED_FLUSH;
+    }
     /* Read in sensor data at 100 Hz and log. */
-    queue.scheduleFunction(sample_sensors, "SAMPDATA", 0, TASK_100HZ);
+    if (queue.scheduleFunction(sample_sensors, "LOG", 100, TASK_100HZ) < 0) {
+        init_success ^= FAULT_SCHED_SAMPL;
+    }
+
 #ifdef DEBUG
-    Serial.printLn("Setup scheduler.")
+    /* Initialize debugging and alert to setup faults. */
+    Serial.begin(9600);
+    if (init_success & FAULT_SDIO) {
+        Serial.println("FAULT: failed to access SD card.");
+    }
+    if (init_success & FAULT_FILEIO) {
+        Serial.println("FAULT: failed to open SD card file(s).");
+    }
+    if (init_success & FAULT_SCHED_FLUSH) {
+        Serial.println("FAULT: failed to schedule flush_logfile().");
+    }
+    if (init_success & FAULT_SCHED_SAMPL) {
+        Serial.println("FAULT: failed to schedule sample_sensors().");
+    }
 #endif
 }
 
@@ -101,7 +112,7 @@ void
 loop()
 {
     /* Only run the loop code if we initialized successfully. */
-    if (init_success) {
+    if (!init_success) {
         /* Start scheduler. */
         queue.Run(millis());
         delay(TASK_100HZ);
@@ -118,7 +129,6 @@ open_logfile(void)
 {
     int8_t rv = TASK_SUCCESS;
     uint8_t num = 0;
-    File fd;
 
     /*
      * Attempt to open the increment file and read in the integer value for
@@ -127,27 +137,28 @@ open_logfile(void)
      * file doesn't exist, we'll just create it.
      */
     if (SD.exists(INCREMENT_FILE)) {
-       fd = SD.open(INCREMENT_FILE, FILE_READ);
-        if (fd.available()) {
-            num =fd.parseInt();
+        if (fd = SD.open(INCREMENT_FILE, FILE_READ)) {
+            if (fd.available()) {
+                num =fd.parseInt();
+            }
+            fd.close();
         }
-       fd.close();
     }
 
     /* Increment the log file number and write to the increment file. */
     ++num;
-   fd = SD.open(INCREMENT_FILE, FILE_WRITE);
-    if (fd) {
+    SD.remove(INCREMENT_FILE);
+    if (fd = SD.open(INCREMENT_FILE, FILE_WRITE)) {
+        fd.print(num);
+        fd.close();
+    } else {
         /* Failed to open increment file for writing. */
         rv = -EIO;
     }
-   fd.write(num);
-   fd.close();
 
     /* Open file for logging. */
-    String logfn = String("flightlog") + String(num) + String(".csv");
-    logfile = SD.open(logfn.c_str(), FILE_WRITE);
-    if (!logfile) {
+    String logfn = String(num) + ".txt";
+    if (!(fd = SD.open(logfn.c_str(), FILE_WRITE))) {
         /* Failed to open new log file. */
         rv = -EIO;
     }
@@ -160,8 +171,8 @@ open_logfile(void)
 int
 flush_logfile(unsigned long now)
 {
-    if (logfile != NULL) {
-        logfile.flush();
+    if (fd != NULL) {
+        fd.flush();
     }
     return TASK_SUCCESS;
 }
@@ -175,9 +186,16 @@ sample_sensors(unsigned long now)
     vn200.read();
     uadc.read();
 #ifdef DEBUG
-    String sep = String(",");
-    Serial.printLn(String("VN200: ") + vn200.roll_s() + sep + vn200.pitch_s() + sep + vn200.yaw_s() + sep + vn200.p_s() + sep + vn200.q_s() + sep + vn200.r_s());
-    Serial.printLn(String("UADC: ") + sep + uadc.airspeed_s() + sep + uadc.alpha_s() + sep + uadc.beta_s());
+    Serial.println(String("VN200: ") + vn200.roll_s() + sep + vn200.pitch_s()
+                   + sep + vn200.yaw_s() + sep + vn200.p_s() + sep
+                   + vn200.q_s() + sep + vn200.r_s());
+    Serial.println(String("UADC : ") + sep + uadc.airspeed_s() + sep
+                   + uadc.alpha_s() + sep + uadc.beta_s());
 #endif
+    fd.println(String("VN200: ") + vn200.roll_s() + sep + vn200.pitch_s()
+               + sep + vn200.yaw_s() + sep + vn200.p_s() + sep + vn200.q_s()
+               + sep + vn200.r_s());
+    fd.println(String("UADC : ") + sep + uadc.airspeed_s() + sep
+               + uadc.alpha_s() + sep + uadc.beta_s());
     return TASK_SUCCESS;
 }
